a02364d03bbbe0368ae00ac9e17904fd8cd03650
rohitamale18 <rohitamale@gmail.com>
2026-07-31 15:40:33 -0700

    fixed a bug with one day ago stock price, added formatting

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-
 from src.metrics import get_purchased_stocks_with_metrics
 from src.ui import get_stock_portfolio_table, get_top_daily_movers
 import streamlit as st
 import pandas as pd
 from src.db import get_stock_purchase_history
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     st.set_page_config(page_title="Rohit's Portfolio", layout="wide")
     st.title("Rohit's Portfolio")
-    
+
     stock_purchase_history = get_stock_purchase_history()
-    purchased_stocks_with_metrics = get_purchased_stocks_with_metrics(stock_purchase_history)
-    
-    get_top_daily_movers(purchased_stocks_with_metrics, 'gainer')
-    get_top_daily_movers(purchased_stocks_with_metrics, 'loser')
-    
+    purchased_stocks_with_metrics = get_purchased_stocks_with_metrics(
+        stock_purchase_history
+    )
+
+    get_top_daily_movers(purchased_stocks_with_metrics, "gainer")
+    get_top_daily_movers(purchased_stocks_with_metrics, "loser")
+
     get_stock_portfolio_table(purchased_stocks_with_metrics)
--- src/metrics.py
+++ src/metrics.py
@@ -1,45 +1,79 @@
 import pandas as pd
-from src.market import (get_stock_price_data, get_latest_price_for_purchased_stocks, get_previous_day_stock_price)
+from src.market import (
+    get_latest_price_for_purchased_stocks,
+)
+
 
 def add_simple_moving_avg(data: pd.DataFrame, days: int) -> pd.DataFrame:
-    
-    data['{days}_moving_avg'.format(days=days)] = data['Close'].rolling(window=days).mean()
+
+    data["{days}_moving_avg".format(days=days)] = (
+        data["Close"].rolling(window=days).mean()
+    )
     return data
 
+
 def get_stock_price_delta(stock_data: pd.DataFrame) -> pd.DataFrame:
-    stock_data['delta'] = stock_data['Close'] - stock_data['avg_purchase_price']
-    stock_data['delta_pct'] = (stock_data['Close'] - stock_data['avg_purchase_price']) / stock_data['avg_purchase_price']
+    stock_data["delta"] = stock_data["Close"] - stock_data["avg_purchase_price"]
+    stock_data["delta_pct"] = (
+        stock_data["Close"] - stock_data["avg_purchase_price"]
+    ) / stock_data["avg_purchase_price"]
 
     return stock_data
+
 
 def get_total_return(stock_data: pd.DataFrame) -> pd.DataFrame:
-    stock_data['total_return'] = 1.0 * stock_data['delta_pct'] * stock_data['total_quantity'] * stock_data['avg_purchase_price']
+    stock_data["total_return"] = (
+        1.0
+        * stock_data["delta_pct"]
+        * stock_data["total_quantity"]
+        * stock_data["avg_purchase_price"]
+    )
     return stock_data
-    
+
+
 def update_sort_stock_data_by_delta(stock_data: pd.DataFrame) -> pd.DataFrame:
     stock_data_sorted = stock_data.copy()
-    stock_data_sorted = stock_data_sorted.sort_values(by=['total_return'], ascending=False)
+    stock_data_sorted = stock_data_sorted.sort_values(
+        by=["total_return"], ascending=False
+    )
     return stock_data_sorted
+
 
 def get_daily_delta(stock_data: pd.DataFrame) -> pd.DataFrame:
     stock_data_copy = stock_data.copy()
-    stock_data_copy['todays_change'] = stock_data_copy['Close'] - stock_data_copy["Previous Close"]
-    stock_data_copy['todays_change_pct'] = 1.0 * (stock_data_copy['Close'] - stock_data_copy["Previous Close"]) / stock_data_copy["Previous Close"]
-    
+    stock_data_copy["todays_change"] = (
+        stock_data_copy["Close"] - stock_data_copy["Previous Close"]
+    )
+    stock_data_copy["todays_change_pct"] = (
+        1.0
+        * (stock_data_copy["Close"] - stock_data_copy["Previous Close"])
+        / stock_data_copy["Previous Close"]
+    )
+
     return stock_data_copy
 
+
 def get_daily_return(stock_data: pd.DataFrame) -> pd.DataFrame:
-    stock_data['daily_return'] = 1.0 * stock_data['todays_change_pct'] * stock_data['total_quantity'] * stock_data['avg_purchase_price']
+    stock_data["daily_return"] = (
+        1.0
+        * stock_data["todays_change_pct"]
+        * stock_data["total_quantity"]
+        * stock_data["avg_purchase_price"]
+    )
     return stock_data
 
 
 def get_purchased_stocks_with_metrics(purchased_stocks: pd.DataFrame) -> pd.DataFrame:
 
-    purchased_stocks_with_metrics = get_latest_price_for_purchased_stocks(purchased_stocks)
+    purchased_stocks_with_metrics = get_latest_price_for_purchased_stocks(
+        purchased_stocks
+    )
     purchased_stocks_with_metrics = get_stock_price_delta(purchased_stocks_with_metrics)
     purchased_stocks_with_metrics = get_total_return(purchased_stocks_with_metrics)
     purchased_stocks_with_metrics = get_daily_delta(purchased_stocks_with_metrics)
     purchased_stocks_with_metrics = get_daily_return(purchased_stocks_with_metrics)
-    purchased_stocks_with_metrics = update_sort_stock_data_by_delta(purchased_stocks_with_metrics)
-    
+    purchased_stocks_with_metrics = update_sort_stock_data_by_delta(
+        purchased_stocks_with_metrics
+    )
+
     return purchased_stocks_with_metrics
